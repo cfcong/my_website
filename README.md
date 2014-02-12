0946380f78de729b8f91c28617c258c670af609a
Colin Fcong <cfcong@gmail.com>
2014-02-12 00:23:31 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+== README
+
+My personal website which consist of my portfolio. [IN PROGRESS]
+
+Things working on:
+
+* Wireframe [February 13]
+
+* Prototype [February 17]
+
+* Back end [February 24]
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mywebsite::Application.config.secret_key_base = 'b915013f40e1d5971a0d52c9b640a64059846fc36eade083acd229ba813776c52b872ee818ea7f1fc5507bea8d052e94918b5346e9d97d9f61a41c98ae8a7136'
